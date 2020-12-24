210b1462317b9bc676d3127214b180a8da601842
Nguyễn Hữu Minh Khai <17520605@gm.uit.edu.vn>
2020-12-24 10:29:13 +0700

    Nguyễn Hữu Minh Khai
    24/12

---
+++ app/src/main/java/Activity/ForgotPasswordActivity.java
@@ -0,0 +1,16 @@
+package Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tutorial_v1.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fogotpassword);
+    }
+}
